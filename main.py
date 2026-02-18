63e8b7ca96473bbd027cd1719c91af6b64c960ee
P0rtillosETY <dsaltyp0rtillo@gmail.com>
2026-02-18 01:31:37 +0000

    Versoón final 1

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+# Carga de los datos del dataset MNIST
 import mnist_loader
 import network
 
-# Cargar datos
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
-# Inicializar red (784 entradas, 30 ocultas, 10 salidas)
+# Configuración de mi red: 784 entradas, 30 neuronas ocultas y 10 de salida
 net = network.Network([784, 30, 10])
 
-# Entrenar
+# Entrenamiento con 30 épocas y tasa de aprendizaje de 3.0
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
